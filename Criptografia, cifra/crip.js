88f1558e0dc88926422b4c02fff96121b71f24e2
Felipe Santiago <felipesantiago@usp.br>
2020-10-09 20:30:05 -0300

    parte dois

--- Criptografia, cifra/crip.js
+++ Criptografia, cifra/crip.js
@@ -25,9 +25,25 @@ function decifraCifraDeCesar( deslocamento, mensagemCifrada )
 {
  var mensagemDecifrada = "";
 
+ for (let letra of mensagemCifrada){
+
+    if (deslocamento > 26){
+        deslocamento = 26;
+    }
+    var letraNumber = letra.charCodeAt() - deslocamento;
+
+    if (letraNumber < 65){
+        letraNumber = letraNumber + 26;
+    }
+
+    mensagemDecifrada +=  String.fromCharCode(letraNumber);
+ }
+
+ 
 
  return mensagemDecifrada;
 }
 
+console.log(decifraCifraDeCesar(1, "GFMJQFA"))
 
 //String.fromCharCode()//
